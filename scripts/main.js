f528ed6b79ebf889468e23b30d77a9eafb7f6c70
daniel ortiz <danielortizdavid@gmail.com>
2021-09-18 21:21:23 -0500

    2 commit

--- scripts/main.js
+++ scripts/main.js
@@ -5,3 +5,25 @@
     dynamicBullets: true,
   },
 });
+/*funcionalidad gsap icono apple*/ 
+gsap.from(".box", {
+  duration: 3,
+  scale: 0.5, 
+  opacity: 0, 
+  delay: 0.5, 
+  stagger: 0.2,
+  ease: "elastic", 
+  force3D: true
+});
+
+document.querySelectorAll(".box").forEach(function(box) {
+  box.addEventListener("click", function() {
+    gsap.to(".box", {
+      duration: 0.5, 
+      opacity: 0, 
+      y: -100, 
+      stagger: 0.1,
+      ease: "back.in"
+    });
+  });
+});
